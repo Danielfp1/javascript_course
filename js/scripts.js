5755f1df800a8d7d959ff4f256b5dc70e9e5be9c
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-12-08 11:31:07 -0300

    Add Lesson 5

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,18 @@
 //Lesson 4 - Working with the DOM
 // *******************************
 
-console.log(document.getElementById("blue_box").innerHTML);
+// console.log(document.getElementById("blue_box").innerHTML);
 
-document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+// document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
 
-console.log(document.getElementById("blue_box").innerHTML);
+// console.log(document.getElementById("blue_box").innerHTML);
+
+
+//********************************
+//Lesson 5 - Variables
+// *******************************
+
+
+var user_name = "Daniel";
+
+document.getElementById("user_greeting_message").innerHTML = "Hello " + user_name + "!";
