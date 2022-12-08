496ffb520dcb77762e32046bacdf82ff64d6823e
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-12-08 11:08:04 -0300

    Add Lesson 4

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,15 @@
 
 //Lesson 3 - Javascript Syntax
 
-console.log("Statement 1")
-console.log("Statement 2")
+// console.log("Statement 1")
+// console.log("Statement 2")
+
+//********************************
+//Lesson 4 - Working with the DOM
+// *******************************
+
+console.log(document.getElementById("blue_box").innerHTML);
+
+document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+
+console.log(document.getElementById("blue_box").innerHTML);
