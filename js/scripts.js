f14ffd6333560f5c41d4acb5c9217dae4693d8e9
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-12-09 08:35:05 -0300

    Add Lesson 17

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,41 @@
 // var user_name = "Daniel";
 
 // document.getElementById("user_greeting_message").innerHTML = "Hello " + user_name + "!";
+
+//********************************
+//Lesson 17 - Data types - Objects
+// *******************************
+
+// var employee = {
+//     'name': 'James Taylor',
+//     'yearOfBirth': 1948,
+//     'ID': 'SBJ0001',
+//     'role': 'IT Analyst'
+// };
+
+// employee.role = "IT Manager";
+// employee.passport = "1023456";
+
+
+var courses = [{
+        'title': 'Learn code in Python 3',
+        'reviews': 6802,
+        'students': 130129,
+        'categories': ['programming', 'technology']
+    },
+
+    {
+        'title': 'Learn PHP -  Beginner to Advanced',
+        'reviews': 1204,
+        'students': 30521,
+        'categories': ['web development', 'programming']
+    },
+
+    {
+        'title': 'Learn Microsoft Excel 2020',
+        'reviews': 4209,
+        'students': 18560,
+        'categories': ['productivity', 'business']
+    }
+
+];
